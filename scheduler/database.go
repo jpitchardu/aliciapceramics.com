e09f965775add53d019a3ec49af613e28f9e3029
Jonathan Pichardo <j.pitchardo@gmail.com>
2025-10-19 01:01:11 -0500

    Remove the body from the response

--- scheduler/database.go
+++ scheduler/database.go
@@ -103,7 +103,7 @@ func GetNonDeadlineOrders() ([]OrderDB, error) {
 	err = json.Unmarshal(body, &orders)
 
 	if err != nil {
-		return []OrderDB{}, fmt.Errorf("failed to parse response: %w, with body %s", err, string(body))
+		return []OrderDB{}, fmt.Errorf("failed to parse response for non deadline orders: %w", err)
 	}
 
 	return orders, nil
