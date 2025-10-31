5b8bd50e2fba8b81f288c9a99d7b96b1b581c6d8
Jonathan Pichardo <j.pitchardo@gmail.com>
2025-10-31 17:31:12 -0500

    Remove method check in schedule tasks

--- api/scheduleTasks.go
+++ api/scheduleTasks.go
@@ -8,11 +8,6 @@ import (
 
 func ScheduleTasksHandler(w http.ResponseWriter, r *http.Request) {
 
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
 	if err := scheduler.Run(); err != nil {
 		LogError("schedule_tasks", err, map[string]any{})
 		http.Error(w, err.Error(), http.StatusInternalServerError)
